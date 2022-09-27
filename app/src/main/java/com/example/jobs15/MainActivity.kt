b2763d0f951a0d998d54d32fd696c7616ff7edd8
fifitt <tiarabintang@gmail.com>
2022-09-27 13:40:37 +0700

    first commit

--- app/src/main/java/com/example/jobs15/MainActivity.kt
+++ app/src/main/java/com/example/jobs15/MainActivity.kt
@@ -16,11 +16,13 @@ class MainActivity : AppCompatActivity() {
 
         tombol.setOnClickListener {
             Toast.makeText(this, "NATASYA AYU L.", Toast.LENGTH_SHORT).show()
+        }
 
         val tombolkls = findViewById<Button>(R.id.tombolKELAS)
 
         tombolkls.setOnClickListener {
-            Toast.makeText( this, "XI PPLG 2", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "XI PPLG 2", Toast.LENGTH_SHORT).show()
+        }
 
         val tombolnis = findViewById<Button>(R.id.tombolnis)
             tombolnis.setOnClickListener {
@@ -28,5 +30,4 @@ class MainActivity : AppCompatActivity() {
             }
         }
         }
-    }
-}
+    
